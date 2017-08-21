e44e5bdd1295be0fb0d12e37ac48eab6bdfc0326
A CHARAN REDDY <31070781+acharanreddy@users.noreply.github.com>
2017-08-21 10:50:23 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,26 @@
 console.log('Loaded!');
+var button=document.getElementById('counter');
+
+button.onclick=function(){
+    //create a request object
+    var request=new XMLHttpRequest();
+    //capturing the response and store it in the variable
+    request.onreadystatechange=function(){
+        if(request.readystate===XMLHttpRequest.DONE)
+        {//take some ACTION
+        if(request.status==200)
+        {
+         var counter=request.responseText;
+         var span=document.getElementById('count')
+         spam.innerHTML=counter.toString();
+         }
+        }
+        
+    };
+    //making the request
+    request.open('GET','http://charanreddyanumula.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    
+    
+}
