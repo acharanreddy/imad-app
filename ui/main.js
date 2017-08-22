594e2c813b7984cbfabafe86aa03755b24c6ea8f
A CHARAN REDDY <31070781+acharanreddy@users.noreply.github.com>
2017-08-22 07:45:14 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,67 @@
 
 var button=document.getElementById('counter');
 var counter=0;
-button.onclick=function () {
+/* button.onclick=function () {
   counter=counter+1;
   var s=document.getElementById('count');
   s.innerHTML=counter.toString();
+}; */
+ button.onclick=function(){
+    //create a request object
+    var request=new XMLHttpRequest();
+    //capturing the response and store it in the variable
+    request.onreadystatechange=function(){
+        if(request.readystate===XMLHttpRequest.DONE)
+        {//take some ACTION
+        if(request.status==200)
+        {
+         var counter=request.responseText;
+         counter=counter+1;
+         var span=document.getElementById('count');
+         span.innerHTML=counter.toString();
+         }
+        }
+        
+    };
+    //making the request
+    request.open('GET','http://charanreddyanumula.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
+
+
+
+//->names-code:
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+   /* var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+        {list+='<li>'+names[i]+'</li>';}
+ //->linkling submit and webpage
+ */
+ 
+ //create a request object
+    var request=new XMLHttpRequest();
+    //capturing the response and store it in the variable
+    request.onreadystatechange=function(){
+        if(request.readystate===XMLHttpRequest.DONE)
+        {//take some ACTION
+        if(request.status==200)
+        {//capture a list of names and rendure them
+          var list='';
+         for(var i=0;i<names.length;i++)
+        {list+='<li>'+names[i]+'</li>';
+         }
+         
+        var ul=document.getElementById('namelist');
+         ul.innerHTML=list;}
+
+        }
+        
+    };
+    //making the request
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+    request.open('GET','http://charanreddyanumula.imad.hasura-app.io/submit-name?name='+name,true);
+         request.send(null);
 };
